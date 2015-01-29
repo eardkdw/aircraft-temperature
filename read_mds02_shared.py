3d2e7b7669c396bb9f6da3ef023522c22e053f5c
Dan Walker <daniel.walker@ncas.ac.uk>
2015-01-29 15:35:48 +0000

    Class-ify aircraft reader

--- read_mds02_shared.py
+++ read_mds02_shared.py
@@ -8,43 +8,63 @@
 # to force a particular type for time_t (it varies with system)
 
 import sysv_ipc
-from mds02 import *
-from ctypes import *
+from mds02 import * #supplies Misc, Plane, P_MAX
+from ctypes import memmove, addressof
 
-shared_memory_id=18087947 # integer 
+#shared_memory_id=50626617 # integer 
 
-TEMP_CONST=38.967854 # from http://www.csgnetwork.com/machonecalc.html
 
-imem = sysv_ipc.attach(shared_memory_id)
-misc = Misc()
-
-sizeofplane=sizeof(Plane) 
-#sizeofplane= #for some reason
-
-#Structure of shared memory is P_MAX Plane instances followed my a Misc
-
-#unpack Misc
-misc_address = imem.address + (P_MAX + 1)*sizeofplane
-memmove(addressof(misc), misc_address, sizeof(Misc))
-
-P_MAX_C= misc.P_MAX_C
-planes = []
-
-#unpack plane (0 is dummy empty record?)
-for i in range (0, P_MAX_C):
-   planes.append(Plane())
-   pi = addressof(planes[i])
-   memmove(pi, imem.address + i *sizeofplane, sizeofplane)
-
-#memstring=imem.read()
-#print memstring.find('MUSIC')
-#print memstring.find('EZY89JU')
-
-print 'Ident, Latitude, Longitude, Altitude / ft, T / °C'
-
-for i in range (0, P_MAX_C):
-   if(planes[i].id <> 0):
-      TK = (planes[i].bds.tas_50/(planes[i].bds.mach_60*TEMP_CONST))**2
-      TC = TK - 273.15
+class AircraftTemp():
+   TEMP_CONST=38.967854 # from http://www.csgnetwork.com/machonecalc.html
+   CELSIUS_OFFSET = 273.15 #offsel vs Kelvin
   
-      print '%9s, %6.4f, %6.4f, %6.0d, %4.2f' % (planes[i].acident, planes[i].lat, planes[i].lon, planes[i].alt, TC)
+   def __init__(self, shared_memory_id): 
+      self.imem = sysv_ipc.attach(shared_memory_id)
+      self.misc = Misc()
+   
+      self.sizeofplane=sizeof(Plane) 
+      self.unpack()
+   
+   #Structure of shared memory is:
+   # empty Plane instance, P_MAX Plane instances, Misc
+   
+   def unpack(self):
+      #unpack Misc
+      misc_address = self.imem.address + (P_MAX + 1)*self.sizeofplane
+      memmove(addressof(self.misc), misc_address, sizeof(Misc))
+      
+      self.P_MAX_C= self.misc.P_MAX_C
+      self.planes = []
+   
+      #unpack plane (0 is dummy empty record?)
+      for i in range (0, self.P_MAX_C):
+         self.planes.append(Plane())
+         pi = addressof(self.planes[i])
+         memmove(pi, self.imem.address + i *self.sizeofplane, self.sizeofplane)
+      
+      #memstring=imem.read()
+      #print memstring.find('MUSIC')
+      #print memstring.find('EZY89JU')
+   
+      #print 'Ident, Latitude, Longitude, Altitude / ft, T / °C'
+   def airtemp(self, plane):
+      TK = (plane.bds.tas_50/(plane.bds.mach_60*self.TEMP_CONST))**2
+      return TK - self.CELSIUS_OFFSET
+
+   def position_temperature(self):
+      out = []
+      for i in range (0, self.P_MAX_C):
+         out.append([self.planes[i].acident, self.planes[i].lat, self.planes[i].lon, self.planes[i].alt, self.airtemp(self.planes[i])]);
+      return out
+         
+
+with open('mds02.log') as f:
+   for line in f:
+      if 'SHM_ID=' in line:
+         print line
+         shm_id = int(line[18:-4])
+         at = AircraftTemp(shm_id)
+         for ac in at.position_temperature():
+            print '%9s, %6.4f, %6.4f, %6.0d, %4.2f' % (ac[0], ac[1], ac[2], ac[3], ac[4])
+            
+
